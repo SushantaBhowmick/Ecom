e5d768283eaf085caca893819a397f8ce4d1b0f6
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-04-19 22:39:19 +0530

    create forgetPassword funcitonality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -61,18 +61,18 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
 };
 
 //Generating Password Reset Token
-userSchema.methods.getResetPasswordToken = function(){
+userSchema.methods.getResetPasswordToken = function (password) {
 
     //Generating token 
     const resetToken = crypto.randomBytes(20).toString("hex");
 
     //Hashing and adding resetPasswordToken to userSchema
     this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
 
-    this.resetPasswordExpire = Date.now() + 15*60*1000;
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
     return resetToken;
 }
 
