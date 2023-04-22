c2e7f53fd43642e297c0e4667a728f1e194da7d3
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-04-22 22:39:38 +0530

    create order functionality and its continue

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,10 @@ app.use(bodyParser.json())
 //Route Imports
 const product= require('./routes/prodRoute')
 const user= require('./routes/userRoutes');
+const order= require('./routes/orderRoute');
 app.use('/api/v1',product);
 app.use('/api/v1',user);
+app.use('/api/v1',order);
 
 
 app.use(errorHandler)
