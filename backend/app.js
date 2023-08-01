a35ffd64affa1fba79e50f2c410fe8684db912ab
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-08-02 01:42:18 +0530

    again change the path

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const errorHandler = require('./middleware/error')
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 
@@ -39,11 +39,11 @@ app.use('/api/v1', user);
 app.use('/api/v1', order);
 app.use('/api/v1', payment);
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*",(req,res)=>{
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// })
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+})
 
 app.use(errorHandler)
 
