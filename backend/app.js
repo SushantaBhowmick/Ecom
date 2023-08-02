d31731f4764a805df79b1d40c09f750137375e01
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-08-03 04:28:49 +0530

    nothing

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
+  origin: true,
   credentials: true,
 })
 );
