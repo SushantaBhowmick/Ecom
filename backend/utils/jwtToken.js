4ebb6c9b39b53f66c8361122719378acecfd6ef0
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-08-03 04:17:42 +0530

    strict

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -11,7 +11,7 @@ const sendToken = (user, statusCode, res) => {
         ),
         httpOnly: true,
         secure:true,
-        sameSite: 'None',
+        sameSite: 'Strict',
         withCredentials:true,
     }
     res.status(statusCode).cookie("token", token, options).json({
