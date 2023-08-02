08955789a5e6a514bb6ce4d987bc71a160e3fbc7
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-08-03 04:54:30 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,12 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
 app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors({
-  origin:true,
-  credentials:true,
-}));
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(fileUpload());
 
