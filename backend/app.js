f51bcf62d1d6bb5142b11a5dc2dee3c0d198dcd5
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-08-04 03:41:03 +0530

    add build

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
+const path = require("path");
+
 const app = express();
 
 //config
@@ -35,6 +37,12 @@ app.use('/api/v1', products);
 app.use('/api/v1', user);
 app.use('/api/v1', order);
 app.use('/api/v1', payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 app.use(errorHandler)
 
