2caf03b4d4d058da75c1b98bce059baad23e9e68
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-08-09 01:33:09 +0530

    false

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,8 +9,8 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
-        secure:false,
+        httpOnly: false,
+        secure:true,
         sameSite:"None",
         withCredentials:true,
     }
