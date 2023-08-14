967cdadcaf76a3d81c8598bb3d3c557f08f0c2c4
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-08-15 02:12:50 +0530

    secure true

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -3,15 +3,15 @@
 const sendToken = (user, statusCode, res) => {
 
     const token = user.getJWTToken();
-    https://ecommerce-store-g8wi.onrender.com
-    http://localhost:4000
+    // https://ecommerce-store-g8wi.onrender.com
+    // http://localhost:4000
     //options for cookie 
     const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
-        secure:false,
+        httpOnly:true,
+        secure:true,
         withCredentials:true,
     }
     res.status(statusCode).cookie("token", token, options).json({
