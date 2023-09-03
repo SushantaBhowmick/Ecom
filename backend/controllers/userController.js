f79907dbba255560ed7e065ad4e757ec6f27bced
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-09-03 22:03:47 +0530

    add credentials

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -60,7 +60,10 @@ exports.logoutUser = catchAsyncError(async (req, res, next) => {
 
     res.cookie("token", null, {
         expires: new Date(Date.now()),
-        httpOnly: false,
+        httpOnly:false,
+        secure:true,
+        sameSite:"None",
+        withCredentials:true,
     })
 
     res.status(200).json({
